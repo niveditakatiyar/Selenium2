ee526c29e506650d90ecd8ad02f267efa27c3022
niveditakatiyar <60661540+niveditakatiyar@users.noreply.github.com>
2020-04-13 20:29:58 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,17 @@
+package assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Q5 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/home/nivedita/Documents/Testing_Java/Chromedriver/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Frames.html");
+		System.out.println(driver.findElements(By.tagName("iframe")).size());
+
+	}
+
+}
+
